3fb2f410c672cd9ec60af2e07e69c144af358829
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-28 21:11:44 +0530

    update the sql naming convention
    added another field to make sense

--- src/main/java/itsvidhanreddy/JDBCMySQLConnector.java
+++ src/main/java/itsvidhanreddy/JDBCMySQLConnector.java
@@ -7,7 +7,7 @@ public class JDBCMySQLConnector {
     String url = "jdbc:mysql://localhost:3306/users";
     String username = "root";
     String password = "root";
-    String query = "SELECT username FROM UserDetails;";
+    String query = "SELECT id, username FROM user_details;";
 
     try {
       Connection con = DriverManager.getConnection(url, username, password);
@@ -15,8 +15,9 @@ public class JDBCMySQLConnector {
       ResultSet rs = st.executeQuery(query);
 
       while (rs.next()) {
+        int id = rs.getInt("id");
         String name = rs.getString("username");
-        System.out.println(name);
+        System.out.println(id + " : " + name);
       }
 
       con.close();
