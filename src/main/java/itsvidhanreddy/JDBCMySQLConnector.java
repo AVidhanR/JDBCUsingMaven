ac66afac839737e7e2c5145fcb0d9b239f49dbfb
AVidhanR <itsvidhanreddy@gmail.com>
2025-01-28 21:29:00 +0530

    tried out the INSERTION operation onto the db
    DML operation - use executeUpdate() as one is updating the data inside the db

--- src/main/java/itsvidhanreddy/JDBCMySQLConnector.java
+++ src/main/java/itsvidhanreddy/JDBCMySQLConnector.java
@@ -7,12 +7,23 @@ public class JDBCMySQLConnector {
     String url = "jdbc:mysql://localhost:3306/users";
     String username = "root";
     String password = "root";
-    String query = "SELECT id, username FROM user_details;";
+    int effectedRows = 0;
+
+    String displayQuery = "SELECT id, username FROM user_details;";
+
+    String insertQuery = """
+        INSERT INTO user_details VALUES (4, "Sujay", "+92 9332929292");
+        """;
 
     try {
       Connection con = DriverManager.getConnection(url, username, password);
       Statement st = con.createStatement();
-      ResultSet rs = st.executeQuery(query);
+
+      System.out.println("Inserting data into the db...");
+      effectedRows = st.executeUpdate(insertQuery);
+
+      ResultSet rs = st.executeQuery(displayQuery);
+      System.out.println();
 
       while (rs.next()) {
         int id = rs.getInt("id");
@@ -25,6 +36,10 @@ public class JDBCMySQLConnector {
       rs.close();
     } catch (SQLException e) {
       System.out.println("Something went wrong: \n" + e.getMessage());
+    }
+    finally {
+      System.out.println();
+      System.out.println(effectedRows + " is row effected");
     }
   }
 }
